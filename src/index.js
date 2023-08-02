d4d199a5d5035d6f62ee774bd1f86079ad5f1ee0
noelfoka <nomgnenoel@gmail.com>
2023-08-02 19:10:43 +0100

    Added the contact in popup-contct.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import './style.css';
-import getData from './modules/display-homepage';
-import showPopupCategories from './modules/show-popup-category';
+import "./style.css";
+import getData from "./modules/display-homepage";
+import showPopupCategories from "./modules/show-popup-category";
+import showPopupContacts from "./modules/popup-contact";
 
 getData();
 
-const menuCat = document.getElementById('categories');
-menuCat.addEventListener('click', () => {
+const menuCat = document.getElementById("categories");
+menuCat.addEventListener("click", () => {
   showPopupCategories();
+});
+
+const menuCont = document.getElementById("contacts");
+menuCont.addEventListener("click", () => {
+  showPopupContacts();
 });
