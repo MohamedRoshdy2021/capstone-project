73196f1df6cd510cbb992a2a12177df560fd6d9a
noelfoka <nomgnenoel@gmail.com>
2023-08-02 18:18:26 +0100

    Added popup category

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import './style.css';
 import getData from './modules/display-homepage';
+import showPopupCategories from './modules/show-popup-category';
 
 getData();
+
+const menuCat = document.getElementById('categories');
+menuCat.addEventListener('click', () => {
+  showPopupCategories();
+});
