f360cf2a639e8d2fd7e0fb443e7314f02fb48eff
Mohammed roshdy <xmedox20111@gmail.com>
2023-08-03 00:21:29 +0300

    .

--- src/modules/show-comments.js
+++ src/modules/show-comments.js
@@ -1,19 +1,20 @@
 function showCommentsList(id, parent) {
   const apiKey = 'tnE2k6P5BdZ2HCTjbd0V';
-  fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${apiKey}/comments?item_id=${id}`)
+  fetch(
+    `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${apiKey}/comments?item_id=${id}`,
+  )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
 
-      const title = document.createElement('h3');
-      title.className = 'meal-popup-comments-title';
-      title.innerHTML = `Comments (${data.length || 0})`;
-      parent.appendChild(title);
-
-      Object.keys(data).forEach((key) => {
+      data.forEach((comment) => {
+        const title = document.createElement('h3');
+        title.className = 'meal-popup-comments-title';
+        title.innerHTML = `Comments (${data.length || 0})`;
+        parent.appendChild(title);
         const commentRow = document.createElement('p');
         commentRow.className = 'meal-popup-comments-row';
-        commentRow.innerHTML = `${data[key].creation_date} ${data[key].username}: ${data[key].comment} `;
+        commentRow.innerHTML = `${comment.creation_date} ${comment.username}: ${comment.comment} `;
         parent.appendChild(commentRow);
       });
     });
