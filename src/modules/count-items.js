f91cb778883b2a282847e2fa0f768677654caabc
noelfoka <nomgnenoel@gmail.com>
2023-08-03 12:37:16 +0100

    start test comment

--- src/modules/count-items.js
+++ src/modules/count-items.js
@@ -2,8 +2,8 @@ const count = () => {
   const box = document.getElementById('data');
 
   const directChildren = box.children.length;
-  const cat = document.getElementById('categories');
-  cat.innerHTML = `Categories: ${directChildren}`;
+  const cat = document.getElementById('meals');
+  cat.innerHTML = `Meals (${directChildren})`;
 };
 
 export default count;
