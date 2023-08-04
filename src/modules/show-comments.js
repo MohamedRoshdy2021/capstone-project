9c987979a944195dea04e16828d5a8b93bafc70a
noelfoka <nomgnenoel@gmail.com>
2023-08-04 04:54:00 +0100

    fix eslint

--- src/modules/show-comments.js
+++ src/modules/show-comments.js
@@ -1,28 +1,28 @@
 async function showCommentsList(id) {
-  const apiKey = "tnE2k6P5BdZ2HCTjbd0V";
+  const apiKey = 'tnE2k6P5BdZ2HCTjbd0V';
   const response = await fetch(
     `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${apiKey}/comments?item_id=${id}`,
     {
-      method: "GET",
+      method: 'GET',
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        'Content-type': 'application/json; charset=UTF-8',
       },
-    }
+    },
   );
 
   try {
     const data = await response.json();
 
-    const parent = document.querySelector(".meal-popup-comments-container");
-    parent.innerHTML = "";
-    const title = document.createElement("h3");
-    title.className = "meal-popup-comments-title";
+    const parent = document.querySelector('.meal-popup-comments-container');
+    parent.innerHTML = '';
+    const title = document.createElement('h3');
+    title.className = 'meal-popup-comments-title';
     title.innerHTML = `Comments (${data.length || 0})`;
     parent.appendChild(title);
 
     data.forEach((comment) => {
-      const commentRow = document.createElement("p");
-      commentRow.className = "meal-popup-comments-row";
+      const commentRow = document.createElement('p');
+      commentRow.className = 'meal-popup-comments-row';
       commentRow.innerHTML = `${comment.creation_date} ${comment.username}: ${comment.comment} `;
       parent.appendChild(commentRow);
     });
