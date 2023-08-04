6c9b495ac762b2cc1cb9a14254eb559f7a3fc96d
noelfoka <nomgnenoel@gmail.com>
2023-08-04 04:50:36 +0100

    used async and await

--- src/modules/show-comments.js
+++ src/modules/show-comments.js
@@ -1,21 +1,34 @@
-function showCommentsList(id, parent) {
-  const apiKey = 'tnE2k6P5BdZ2HCTjbd0V';
-  fetch(
+async function showCommentsList(id) {
+  const apiKey = "tnE2k6P5BdZ2HCTjbd0V";
+  const response = await fetch(
     `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${apiKey}/comments?item_id=${id}`,
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((comment) => {
-        const title = document.createElement('h3');
-        title.className = 'meal-popup-comments-title';
-        title.innerHTML = `Comments (${data.length || 0})`;
-        parent.appendChild(title);
-        const commentRow = document.createElement('p');
-        commentRow.className = 'meal-popup-comments-row';
-        commentRow.innerHTML = `${comment.creation_date} ${comment.username}: ${comment.comment} `;
-        parent.appendChild(commentRow);
-      });
+    {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
+
+  try {
+    const data = await response.json();
+
+    const parent = document.querySelector(".meal-popup-comments-container");
+    parent.innerHTML = "";
+    const title = document.createElement("h3");
+    title.className = "meal-popup-comments-title";
+    title.innerHTML = `Comments (${data.length || 0})`;
+    parent.appendChild(title);
+
+    data.forEach((comment) => {
+      const commentRow = document.createElement("p");
+      commentRow.className = "meal-popup-comments-row";
+      commentRow.innerHTML = `${comment.creation_date} ${comment.username}: ${comment.comment} `;
+      parent.appendChild(commentRow);
     });
+  } catch (err) {
+    // do nothing
+  }
 }
 
 export default showCommentsList;
